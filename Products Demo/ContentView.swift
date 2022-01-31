bf86d5f77757a49f379a37312f6420ee0161523d
Muhammed Sahil AK <mdsahilak@users.noreply.github.com>
2022-01-31 16:04:55 +0530

    Initial Commit

---
+++ Products Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Products Demo
+//
+//  Created by Muhammed Sahil Arayakandy on 31/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Products Demo/Products_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Products_DemoApp.swift
+//  Products Demo
+//
+//  Created by Muhammed Sahil Arayakandy on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Products_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
