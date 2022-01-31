e5cca1e353215b3590c2312e2bb41c1d75c336e2
Muhammed Sahil AK <mdsahilak@users.noreply.github.com>
2022-01-31 16:14:57 +0530

    Create Data Models

---
+++ Products Demo/DataModels/Product.swift
@@ -0,0 +1,39 @@
+//
+//  Product.swift
+//  Products Demo
+//
+//  Created by Muhammed Sahil Arayakandy on 31/01/22.
+//
+
+import Foundation
+
+struct Product: Codable, Identifiable {
+    var id: UUID = UUID()
+    
+    let productName: String
+    let brandName: String
+    let price: Int
+    let address: Address
+    let description: String
+    let date: String
+    let time: String
+    let imageURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case productName = "product_name"
+        case brandName = "brand_name"
+        
+        case price
+        case address
+        case description
+        case date
+        case time
+        
+        case imageURL = "image"
+    }
+    
+    struct Address: Codable {
+        let state: String
+        let city: String
+    }
+}
